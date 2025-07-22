1c100ae15b9610de7df79f6dfa9a89c8d15daa15
Shams-20 <shamsxshaikh@gmail.com>
2025-07-22 11:32:59 +0530

    save baseline hashes to JSON file

--- file_integrity_checker.py
+++ file_integrity_checker.py
@@ -1,5 +1,6 @@
 import os
 import hashlib
+import json
 
 def get_all_files(directory):
     file_paths = []
@@ -14,3 +15,10 @@ def calculate_hash(filepath):
         for chunk in iter(lambda: f.read(4096), b''):
             hasher.update(chunk)
     return hasher.hexdigest()
+
+def save_hashes(hash_dict, output_file):
+    with open(output_file, 'w') as f:
+        json.dump(hash_dict, f, indent=4)
+
+
+
