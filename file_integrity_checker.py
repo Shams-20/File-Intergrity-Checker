1bf46aae5ee32ef32b601fb2cd080275d919a69f
Shams-20 <shamsxshaikh@gmail.com>
2025-07-22 11:02:51 +0530

    adding file hashing function using sha256

--- file_integrity_checker.py
+++ file_integrity_checker.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 
 def get_all_files(directory):
     file_paths = []
@@ -6,3 +7,10 @@ def get_all_files(directory):
         for filename in files:
             file_paths.append(os.path.join(root, filename))
     return file_paths
+
+def calculate_hash(filepath):
+    hasher = hashlib.sha256()
+    with open(filepath, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
+            hasher.update(chunk)
+    return hasher.hexdigest()
